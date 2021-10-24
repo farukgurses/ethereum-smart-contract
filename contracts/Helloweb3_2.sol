8e20341d17962402d8976cb9d7ba18935f513acf
Ahmet Faruk Gürses <afarukg@gmail.com>
2021-10-24 15:57:49 +0300

    added secont contract and interacted with first one

---
+++ contracts/Helloweb3_2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Helloweb3_2{
+    string public message;
+
+    constructor(string memory _message){
+        message = _message;
+    }
+}
--- migrations/2_hello_web3_migration.js
+++ migrations/2_hello_web3_migration.js
@@ -1,5 +1,10 @@
 const Helloweb3 = artifacts.require("Helloweb3");
+const Helloweb3_2 = artifacts.require("Helloweb3_2");
 
 module.exports = function (deployer) {
-  deployer.deploy(Helloweb3, "Welcome to our world!");
+  deployer.deploy(Helloweb3, "Welcome to our world!").then( async()=>{
+    let instance = await Helloweb3.deployed();
+    let message = instance.message()
+    return deployer.deploy(Helloweb3_2, message)
+  })
 };
